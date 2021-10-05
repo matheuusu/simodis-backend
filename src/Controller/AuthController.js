0d71e6f94387fe852d021d074c4cca272dbe8e97
Matheus Silva <54555111+matheuusu@users.noreply.github.com>
2021-10-05 00:47:56 -0300

    05/10

--- src/Controller/AuthController.js
+++ src/Controller/AuthController.js
@@ -8,15 +8,15 @@ module.exports = {
     signin: async (req, res) => {
         //Validando que os campos foram, devidamente, inseridos
         const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.json({ error: errors.mapped() });
+        if(!errors.isEmpty()){
+            res.json({error: errors.mapped()});
             return;
         }
 
-        const data = matchedData(req);
-
+        const data = matchedData(req);        
+        
         //Consultando se o email é válido, isto é, se está cadastrado. Equivale ao SELECT * FROM users WHERE email = req.email
-        const user = await Usuarios.Usuarios.findOne({
+        const user = await Usuarios.Usuarios.findOne({            
             where: {
                 email: data.email
             }
@@ -24,15 +24,15 @@ module.exports = {
 
         //Se user for NULL, o Backend retorna a mensagem de erro.
         //Validando usuário
-        if (!user) {
-            res.json({ error: 'Email e/ou senha inválidos!' });
+        if(!user){
+            res.json({error: 'Email e/ou senha inválidos!'});
             return;
         }
 
         //Validando a senha do usuário
         const passwordCheck = await bcrypt.compare(data.password, user.password);
-        if (!passwordCheck) {
-            res.json({ error: 'Email e/ou senha inválidos!' });
+        if(!passwordCheck){
+            res.json({error: 'Email e/ou senha inválidos!'});
             return;
         }
 
@@ -44,60 +44,59 @@ module.exports = {
         //Salvando as alteração do token
         await user.save();
 
-        res.json({ token: user.token });
+        res.json({token: user.token});
     },
 
     signup: async (req, res) => {
         //Validando que os campos foram, devidamente, inseridos
         const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.json({ error: errors.mapped() });
+        if(!errors.isEmpty()){
+            res.json({error: errors.mapped()});
             return;
         }
-
+    
         const data = matchedData(req);
 
-        // Consultando se o email é válido, isto é, se está cadastrado. Equivale ao SELECT * FROM users WHERE email = req.email
-        const userCheck = await Usuarios.Usuarios.findOne({
+        //Consultando se o email é válido, isto é, se está cadastrado. Equivale ao SELECT * FROM users WHERE email = req.email
+        const userCheck = await Usuarios.Usuarios.findOne({            
             where: {
                 email: data.email
             }
         });
 
-        if (userCheck) {
-            res.json({ error: 'Email já cadastrado!' });
+        if(userCheck){
+            res.json({error: 'Email já cadastrado!'});
             return;
-
         }
-        
+
         //Criptografando Senha do usuário
         const password = await bcrypt.hash(data.password, 10);
-
+        
         //Concatenando Data atual mais Número Randómico para o Hash
         const payload = (Date.now() + Math.random()).toString();
         //Gerando Hash 10 para o token de autenticação
-        const token = await bcrypt.hash(payload, 10);
-
+        const token = await bcrypt.hash(payload, 10);  
+        
         // Se a tabela não existir, ele cria automaticamente. Por isso, o métod sync() não tem parâmetros.
         // Caso a tabela já exista, ele não faz nada. Se precisar forçar a criação de várias, utilizar o método assim: sync({ force: true})
         Usuarios.Usuarios.sync();
-
+        
         //Inserindo Registro no banco com esta estrutura
         const user = Usuarios.Usuarios.build({
             name: data.name,
             password,
-            enrollment: data.enrollment,
+            enrollment: data.enrollment,            
             token,
-            email: data.email
-
+            email: data.email            
+    
         })
         //Salvando Instancia do Model Usuarios no Banco, garantindo persistência dos dados
         await user.save();
         console.log("Usuário cadastrado com sucesso!");
-        console.log(user.email);
-
+        console.log(user.email);        
+    
         //Resposta da Requisição na Aplicação
-        res.json({ token });
+        res.json({token});
     }
-
+    
 }
--- src/Dao/DB.js
+++ src/Dao/DB.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(`${process.env.BANCO}`, `${process.env.USER}`, `${process.env.PASSWORD}`, {
-    host: 'localhost',
+    host: 'db4free.net',
     dialect: 'mysql'
 })
 
