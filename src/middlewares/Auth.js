569f54de59ecd7d7b3b1a700de68dc65df64002b
matheus <devtheus0@gmail.com>
2021-12-09 03:59:06 -0300

    Update Auth.js

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -3,42 +3,37 @@ const Usuarios = require('../Models/Usuarios')
 
 module.exports = {
   private: async (req, res, next) => {
-    const token = req.get('Authorization')
-    // console.log(JSON.stringify(req, null, 2))
+    if(!req.query.token && !req.body.token){
+        res.json({notallowed: true});
+        return;
+    }
+    
+    let token = '';
 
-    if (!token || token === '') {
-      res.status(405).json({ notallowed: true })
-      return
+    if(req.query.token){
+        token = req.query.token;
     }
 
-    // let token = ''
-
-    // if (req.query.token) {
-    //   token = req.query.token
-    // }
-
-    // if (req.body.token) {
-    //   token = req.body.token
-    // }
-
-    // if (token == '') {
-    //   res.json({ notallowed: true })
-    //   return
-    // }
-
-    const user = await Usuarios.Usuarios.findOne({
-      where: {
-        token: token
-      }
-    })
-
-    if (!user) {
-      res.json({ notallowed: true })
-      return
+    if(req.body.token){
+        token = req.body.token;
+    }
+       
+    if(token == ''){
+        res.json({notallowed: true});
+        return;
     }
 
-    req.user = user
+   
+    const user = await Usuarios.Usuarios.findOne({            
+        where: {
+            token: token
+        }
+    });
 
-    next()
-  }
+    if(!user){
+        res.json({notallowed: true});
+        return;
+    }
+
+    next();
 }
