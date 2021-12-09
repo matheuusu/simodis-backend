cbcb25314171b0e7cd59ef39f8889f9cea02b9c4
matheus <devtheus0@gmail.com>
2021-12-09 04:02:58 -0300

    Update Auth.js

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -3,37 +3,37 @@ const Usuarios = require('../Models/Usuarios')
 
 module.exports = {
   private: async (req, res, next) => {
-    if(!req.query.token && !req.body.token){
-        res.json({notallowed: true});
-        return;
-    }
-    
-    let token = '';
-
-    if(req.query.token){
-        token = req.query.token;
+    if (!req.query.token && !req.body.token) {
+      res.json({ notallowed: true })
+      return
     }
 
-    if(req.body.token){
-        token = req.body.token;
-    }
-       
-    if(token == ''){
-        res.json({notallowed: true});
-        return;
+    let token = ''
+
+    if (req.query.token) {
+      token = req.query.token
     }
 
-   
-    const user = await Usuarios.Usuarios.findOne({            
-        where: {
-            token: token
-        }
-    });
-
-    if(!user){
-        res.json({notallowed: true});
-        return;
+    if (req.body.token) {
+      token = req.body.token
     }
 
-    next();
+    if (token == '') {
+      res.json({ notallowed: true })
+      return
+    }
+
+    const user = await Usuarios.Usuarios.findOne({
+      where: {
+        token: token
+      }
+    })
+
+    if (!user) {
+      res.json({ notallowed: true })
+      return
+    }
+
+    next()
+  }
 }
