99098587a0ebcacd6c057054bdbc40782dd3da0e
ivanfigueiredo <77892424+ivanfigueiredo@users.noreply.github.com>
2021-12-12 19:48:31 -0300

    Update QuestionsController.js

--- src/Controller/QuestionsController.js
+++ src/Controller/QuestionsController.js
@@ -17,7 +17,7 @@ module.exports = {
 
     const data = matchedData(req)
 
-    Questions.sync()
+    /*Questions.sync()
 
     const questions = await Questions.findAll({})
     const title = data.title.split(',')
@@ -52,7 +52,8 @@ module.exports = {
       await AnswersController.create(quest.id, '', title[i], false)
     }
 
-    await AnswersController.create(quest.id, data.answer_true, '', true)
+    await AnswersController.create(quest.id, data.answer_true, '', true)*/
+    console.log(data.title "  " + data.answer_true + "  " + data.course_id);
     res.json({OK: true})
   }
 
