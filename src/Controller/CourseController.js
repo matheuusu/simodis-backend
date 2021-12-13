f6e75177a5c39eafed01e45e4fdaddc5800bd815
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-12-13 17:59:34 -0300

    ...........

--- src/Controller/CourseController.js
+++ src/Controller/CourseController.js
@@ -126,7 +126,7 @@ module.exports = {
       /*if(answer.isTrue == 1){
         ++count_success;
       }*/
-      console.log(answer.dataValues)
+      console.log(answer['dataValues'])
     }
 
     res.json({count_success});
