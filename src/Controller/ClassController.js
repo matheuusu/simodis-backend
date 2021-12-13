fc3999ca0c20f962a85d57235ddc53e08ab1376c
ivanfigueiredo <77892424+ivanfigueiredo@users.noreply.github.com>
2021-12-12 22:51:31 -0300

    Update ClassController.js

--- src/Controller/ClassController.js
+++ src/Controller/ClassController.js
@@ -5,7 +5,7 @@ module.exports = {
   create: async (req, res) => {
     let { token, id } = await req.query
 
-    let user = await Usuarios.findOne({
+    /*let user = await Usuarios.findOne({
       where: {
         token: token
       }
@@ -37,7 +37,9 @@ module.exports = {
       course_id: id
     })
 
-    await newUser.save()
+    await newUser.save()*/
+    
+    console.log(token + " " + id);
 
     res.json({ Ok: true })
   },
